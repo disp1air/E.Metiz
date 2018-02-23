846fdebbda1cb8457d8979ac9cc017569d99d12d
disp1air <hola-shola@mail.ru>
2018-02-23 23:50:00 +0300

    add survey tests

---
+++ Ch_15/mpl_squares.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+
+plt.plot(squares, linewidth=5)
+
+# Назначение заголовка диаграммы и меток осей
+plt.title("Square Numbers", fontsize=24)
+
+plt.show()
