201d2999b9d6dd6da1479dc9baa0ba92faa65972
disp1air <hola-shola@mail.ru>
2018-02-24 11:51:35 +0300

    add scatter_squares and ch_15

--- Ch_15/scatter_squares.py
+++ Ch_15/scatter_squares.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values , y_values, s=50)
 
 # Назначение заголовка диаграммы и меток осей.
 plt.title("Square Numbers", fontsize=16)
@@ -9,5 +12,8 @@ plt.ylabel("Square of Value", fontsize=12)
 
 # Назначение размера шрифта делений на осях.
 plt.tick_params(axis='both', which='major', labelsize=10)
+
+# Назначение диапазона для каждой оси.
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
