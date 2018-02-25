6a51ddb60a4c42d5b46a8b37da52c374015582af
disp1air <hola-shola@mail.ru>
2018-02-25 14:00:47 +0300

    add daily high temperatures

--- Ch_16/highs_lows.py
+++ Ch_16/highs_lows.py
@@ -1,8 +1,30 @@
 import csv
+from matplotlib import pyplot as plt
+from datetime import datetime
 
 filename = 'sitka_weather_07-2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
-    header_pow = next(reader)
-    print(header_pow)
+    header_row = next(reader)
+
+    highs, dates = [], []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
+print(highs)
+
+#fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red')
+
+plt.title("Daily high temperatures, July 2014", fontsize=14)
+plt.xlabel('', fontsize=12)
+
+#fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis='both', which='major', labelsize=12)
+
+plt.show()
