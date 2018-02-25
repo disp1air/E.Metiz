c68e35588bf4183bc356a73ed15b08425b0a81f0
disp1air <hola-shola@mail.ru>
2018-02-25 16:22:01 +0300

    ch_16 add sitka_weather

--- Ch_16/highs_lows.py
+++ Ch_16/highs_lows.py
@@ -2,23 +2,32 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
+#filename = 'sitka_weather_07-2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs, dates = [], []
+    highs, lows, dates = [], [], []
     for row in reader:
-        high = int(row[1])
-        highs.append(high)
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-print(highs)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:    
+            print(current_date, 'missing data')
+        else:    
+            highs.append(high)
+            lows.append(low)
+            dates.append(current_date)
+# print(highs)
+# print(dates)
 
 #fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 plt.title("Daily high temperatures, July 2014", fontsize=14)
 plt.xlabel('', fontsize=12)
