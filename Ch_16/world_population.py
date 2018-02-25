0bb40482d6a69af6a7c811bf85f9bc2a2a37da41
disp1air <hola-shola@mail.ru>
2018-02-25 21:56:40 +0300

    add americas and na_populations

--- Ch_16/world_population.py
+++ Ch_16/world_population.py
@@ -1,4 +1,5 @@
 import json
+from country_codes import get_country_code
 
 filename = 'population_data.json'
 
@@ -8,5 +9,9 @@ with open(filename) as f:
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
-        population = int(float(pop_dict['Value'])
-        print(country_name + ': ' + str(population))
+        population = int(float(pop_dict['Value']))
+        code = get_country_code(country_name) 
+        if code:
+            print(code + ': ' + str(population))
+        else:
+            print('ERROR - ' + country_name)
