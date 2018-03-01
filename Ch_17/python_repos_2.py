c57b720e6aaa33e259f8774ad3690768ccad9dba
disp1air <hola-shola@mail.ru>
2018-03-01 20:54:46 +0300

    add svg

--- Ch_17/python_repos_2.py
+++ Ch_17/python_repos_2.py
@@ -14,11 +14,18 @@ print('Total repositories: ', response_dict['total_count'])
 
 # Анализ информации о репозиториях.
 repo_dicts = response_dict['items']
+print("Number of items: ", len(repo_dicts))
 
-names, stars = [], []
+names, stars, plot_dicts = [], [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+plot_dict = {
+    'value': repo_dict['stargazers_count'],
+    'label': repo_dict['description']
+}
+plot_dicts.append(plot_dict)
 
 # Построение визуализации.
 my_style = LS('#333366', base_style=LCS)
@@ -37,5 +44,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
-chart.render_to_file('python_repos.svg')
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos_2.svg')
