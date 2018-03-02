3840f0b62597d34592c5d3e928a4852b91d834d6
disp1air <hola-shola@mail.ru>
2018-03-02 19:42:41 +0300

    add hn_sub

---
+++ Ch_17/hn_submissions.py
@@ -0,0 +1,18 @@
+import requests
+
+from operator import itemgetter
+
+# Создание вызова API и сохранение ответа.
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# Обработка информации о каждой статье.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    # Создание отдельного вызова API для каждой статьи.
+    url = ('https://hacker-news.firebaseio.com/v0/item/' + str(submission_id) + '.json')
+    submission_r = requests.get(url)
+    print(submission_r.status_code)
+    response_dict = submission_r.json()
